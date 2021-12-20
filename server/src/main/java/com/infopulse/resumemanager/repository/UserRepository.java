6388c226eed36d4a365a5a02793999dbe42ea482
alexander-adamenko <alex.adamenko.417@gmail.com>
2021-12-20 18:34:00 +0200

    Fix n+1 problem in UserRepository.

--- server/src/main/java/com/infopulse/resumemanager/repository/UserRepository.java
+++ server/src/main/java/com/infopulse/resumemanager/repository/UserRepository.java
@@ -2,11 +2,19 @@ package com.infopulse.resumemanager.repository;
 
 import com.infopulse.resumemanager.repository.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByUsername(String username);
+
+    @Override
+    @Query("SELECT u FROM User u LEFT JOIN FETCH u.roles")
+    List<User> findAll();
+
 //    @Query("")
 //    void addRoleToUser(String username, String roleName);
 
